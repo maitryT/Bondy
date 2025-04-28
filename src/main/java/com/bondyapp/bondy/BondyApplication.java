9cead4ced58e5ca1920ca5f5535b0cb517371b02
DESKTOP-MR87CNH\CEM03 <maitri.thakkar@ihsmarkit.com>
2025-04-28 16:11:04 +0530

    Spring Initial setup

---
+++ src/main/java/com/bondyapp/bondy/BondyApplication.java
@@ -0,0 +1,13 @@
+package com.bondyapp.bondy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondyApplication.class, args);
+	}
+
+}
